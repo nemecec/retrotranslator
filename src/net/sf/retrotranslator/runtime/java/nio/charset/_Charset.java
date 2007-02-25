fdc198d2e553b46458e421f4791ca400de822eb1
tarasp <tarasp>
2007-02-25 13:27:58 +0000

    *** empty log message ***

---
+++ src/net/sf/retrotranslator/runtime/java/nio/charset/_Charset.java
@@ -0,0 +1,55 @@
+/***
+ * Retrotranslator: a Java bytecode transformer that translates Java classes
+ * compiled with JDK 5.0 into classes that can be run on JVM 1.4.
+ * 
+ * Copyright (c) 2005 - 2007 Taras Puchko
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.sf.retrotranslator.runtime.java.nio.charset;
+
+import java.io.*;
+import java.nio.charset.*;
+
+/**
+ * @author Taras Puchko
+ */
+public class _Charset {
+
+    public static Charset defaultCharset() {
+        try {
+            // The default charset depends on the locale 
+            // and the "file.encoding" property.
+            return Charset.forName(new OutputStreamWriter
+                    (new ByteArrayOutputStream()).getEncoding());
+        } catch (UnsupportedCharsetException e) {
+            // UTF-8 is used on JDK 1.4.0
+            // if the default charset is unavailable.
+            return Charset.forName("UTF-8");
+        }
+    }
+
+}
